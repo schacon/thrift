ad32dd3a420bf613d2331e43b0d427f71f15fb56
David Reiss <dreiss@facebook.com>
2008-10-21 12:39:40 -0700

    junk

--- test/UnitTestMain.cpp
+++ test/UnitTestMain.cpp
@@ -1,2 +1,8 @@
+#include <boost/version.hpp>
 #define BOOST_TEST_MODULE thrift
+#if BOOST_VERSION >= 103400
 #include <boost/test/included/unit_test.hpp>
+#else
+// Use the deprecated header with old boost versions.
+#include <boost/test/included/unit_test_framework.hpp>
+#endif
